6a97966783c05ee5ed5b83ce62aac2e855722a92
dhiraj97 <dhirajramnani86@gmail.com>
2020-04-20 14:41:53 -0400

    1. Added Map Fragment 2. Added Permissions 3. Checked Permissions

--- app/src/main/java/com/example/coronatracker/MainActivity.java
+++ app/src/main/java/com/example/coronatracker/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/coronatracker/NearbyPatientsFragment.java
+++ app/src/main/java/com/example/coronatracker/NearbyPatientsFragment.java
@@ -1,15 +1,37 @@
 package com.example.coronatracker;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class NearbyPatientsFragment extends Fragment {
+    private static final String TAG = "NearbyPatientsFragment";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    public static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    public static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private Boolean mLocationPermissionGranted = false;
+    private GoogleMap mMap;
     public NearbyPatientsFragment() {
     }
 
@@ -17,6 +39,67 @@ public class NearbyPatientsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nearby_patients,container, false);
+        getLocaotionPermission();
+        isServicesOK();
         return view;
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getActivity());
+        if(available == ConnectionResult.SUCCESS){
+            //everything is ok
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        } else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but resolvable
+            Log.d(TAG, "isServicesOK: an error occured, but is fixable");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(getActivity(), available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(getActivity(),"You cannot make Map Request",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    public void getLocaotionPermission(){
+        String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(getActivity(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(getActivity(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            }
+            else{
+                ActivityCompat.requestPermissions(getActivity(), permissions,1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 1:
+                if(grantResults.length > 0){
+                    for(int i = 0; i < grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                    //initialize our map
+                    initMap();
+                }
+        }
+    }
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Toast.makeText(getActivity(),"Map is Ready",Toast.LENGTH_SHORT).show();
+                mMap = googleMap;
+            }
+        });
+
     }
 }
